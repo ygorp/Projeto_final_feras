d5048cb913e7003e88d04b55b1414526fa280d29
ygorp <ygor-nogueira@hotmail.com>
2024-01-22 10:39:12 -0300

    criando um sistema de cadastro de cliente com login para autenticação, feito a página de login e criando a página que lista os clientes

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Feras Tecnologia</title>
+
+    <link rel="stylesheet" href="/css/login.css">
+
+</head>
+<body>
+    <main>
+        <div class="container">
+            <div class="login">
+                <div class="login__logo">
+                    <img src="/img/logo.png" alt="Logo Feras Tecnologia">
+                </div>
+                
+                <div class="login__form">
+                    <form action="/login" method="POST">
+                    <p>Bem-vindo</p>
+                        <div class="login__form__group">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" id="email" placeholder="Digite seu e-mail">
+                        </div>
+                        <div class="login__form__group">
+                            <label for="password">Senha</label>
+                            <input type="password" name="password" id="password" placeholder="Digite sua senha">
+                        </div>
+                        <div class="login__form__group">
+                            <button type="submit">Entrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ pages/clientes.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clientes - Feras</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/css/clientes.css">
+
+</head>
+<body>
+    <header class="topo_clientes">
+        <div class="logo">
+            <img src="/img/logo.png" alt="Logo feras da tecnologia">
+        </div>
+
+        <div class="usuario">
+            <p>Ygor pacheco</p>
+            <img src="/img/logo.png" alt="">
+        </div>
+    </header>
+
+    <main>
+        <div class="secao">
+        <i class="fa-solid fa-user"></i> <p>Clientes</p>
+        </div>
+
+        <div class="cliente_list">
+            <div class="titulo_btn">
+                <h1>Clientes</h1>
+                <button><i class="fa-solid fa-square-plus"></i></button>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
