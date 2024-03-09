f94d3279a392572bd46a95b71c1688756e9753bb
ygorp <ygor-nogueira@hotmail.com>
2024-03-08 22:36:21 -0300

    atualizando o recebimento de dados e exibindo na tela

--- pages/clientes.php
+++ pages/clientes.php
@@ -8,6 +8,10 @@ if (!isset($_SESSION['usuario_email'])) {
 
 
 include("../database/db.php");
+include("../database/conn.php");
+
+$sql = "SELECT * FROM clientes ORDER BY idclientes DESC";
+$result = $conn->query($sql);
 
 ?>
 
@@ -19,6 +23,7 @@ include("../database/db.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clientes - Feras</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/css/clientes.css">
 
@@ -48,32 +53,36 @@ include("../database/db.php");
                 <a href="/pages/cad_clientes.php"><i class="fa-solid fa-square-plus"></i></a>
             </div>
 
-            <table class="estilo-tabela">
-                <tr>
-                    <td>Nome Completo</td>
-                    <td>CPF</td>
-                    <td>Celular</td>
-                    <td>Email</td>
-                    <td>Status</td>
-                    <td>Ação</td>
-                </tr>
-
-                <?php
-                    // Exibe os usuários na tabela
-                    include("../database/conn.php");
-                    $sql = "SELECT * FROM clientes";
-                    $resultado = $conn->query($sql);
-                    while ($row = $resultado->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>{$row['nome']}</td>";
-                        echo "<td>{$row['cpf']}</td>";
-                        echo "<td>{$row['celular']}</td>";
-                        echo "<td>{$row['email']}</td>";
-                        echo "<td>{$row['status']}</td>";
-                        echo "</tr>";
-                    }
-                ?>
-            </table>
+            <div class="table_list">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome Completo</th>
+                            <th scope="col">CPF</th>
+                            <th scope="col">Celular</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($user_data = mysqli_fetch_assoc($result))
+                            {
+                                echo "<tr>";
+                                echo "<td>".$user_data['idclientes']."</td>";
+                                echo "<td>".$user_data['nome']."</td>";
+                                echo "<td>".$user_data['cpf']."</td>";
+                                echo "<td>".$user_data['celular']."</td>";
+                                echo "<td>".$user_data['email']."</td>";
+                                echo "<td>".$user_data['status']."</td>";
+                                echo "</tr>";
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
 </body>
